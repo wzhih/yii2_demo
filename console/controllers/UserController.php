c252d31852a9a9086584fe2ff8791e80326f85c2
wangzhihao <wanzhihao@shxingwan.com>
2020-05-30 10:23:29 +0800

    修改次日留存脚本，每小时运行

--- console/controllers/UserController.php
+++ console/controllers/UserController.php
@@ -16,12 +16,18 @@ class UserController extends Controller
         return 0;
     }
 
-    public function actionDayRetentionRate(string $start = '', string $end = '')
+    /**
+     * 更新时间范围内的新注册人数
+     * 更新时间范围区间 [$start, $end)
+     * @param string $start
+     * @param string $end
+     */
+    public function actionDayRegister(string $start = '', string $end = '')
     {
         //判断是否输入起始日期参数
         if (!$start) {
-            //没有输入，默认取前天
-            $start = Carbon::now()->subDays(2);
+            //没有输入，默认取昨天
+            $start = Carbon::yesterday();
         } else {
             //有输入，解析
             $start = (new Carbon($start))->startOfDay();
@@ -29,7 +35,7 @@ class UserController extends Controller
 
         //判断是否输入结束日期参数
         if (!$end) {
-            $end = Carbon::now()->subDays(1);
+            $end = Carbon::today();
         } else {
             //有输入，解析
             $end = (new Carbon($end))->endOfDay();
@@ -40,54 +46,62 @@ class UserController extends Controller
             throw new Exception('输入日期错误');
         }
 
+        $time = Carbon::now()->getTimestamp();
         while (true) {
-            $this->dayRetentionRate($start);
+            //获取新注册用户数量
+            $registerNum = UserModel::find()
+                ->select('id')
+                ->where(['>=', 'createdAt', $start->startOfDay()->getTimestamp()])
+                ->andWhere(['<=', 'createdAt', $start->endOfDay()->getTimestamp()])
+                ->count();
+
+            //插入或更新对应日期的记录
+            $sql = <<<SQL
+INSERT INTO `t_user_day_retention_rate` ( `day`, `register_num`, `login_num`, `created_at`, `updated_at` )
+VALUES
+	( '{$start->toDateString()}', {$registerNum}, 0, {$time}, {$time} ) 
+	ON DUPLICATE KEY UPDATE register_num = {$registerNum}, updated_at = {$time}
+SQL;
+            Yii::$app->db->createCommand($sql)
+                ->execute();
 
             $start->addDay();
             if (!$start->diffInDays($end)) {
                 break;
             }
         }
-
-        return 0;
     }
 
-    //要计算"day"的留存率，需要查询这一天新注册用户数，这些新注册用户，在隔天的登录数。再相除
-    protected function dayRetentionRate(Carbon $day)
+    /**
+     * 运行间隔：
+     * 更新"昨日"新注册用户，在上一小时的登录用户数
+     * @param string $start
+     * @param string $end
+     */
+    public function actionHourLogin(string $now = '')
     {
-        $start_time = $day->startOfDay()->getTimestamp();
-        $end_time = $day->endOfDay()->getTimestamp();
-
-        //获取"今日"新注册用户id
-        $reg = UserModel::find()
-            ->select('id')
-            ->where(['>=', 'createdAt', $start_time])
-            ->andWhere(['<=', 'createdAt', $end_time])
-            ->column();
-
-        $time = $day->timestamp;
-        $count = count($reg);
-        if ($count == 0) {
-            //新注册数等于零，留存率直接为0%
-            $sql = <<<SQL
-INSERT INTO `t_user_day_retention_rate` ( `day`, `rate`, `created_at`, `updated_at` )
-VALUES
-	( '{$day->toDateString()}', 0, {$time}, {$time} ) 
-	ON DUPLICATE KEY UPDATE rate = 0,
-	updated_at = {$time}
-SQL;
-            Yii::$app->db->createCommand($sql)
-                ->execute();
-            return 0;
+        if (empty($now)) {
+            $now = Carbon::now();
+        } else {
+            $now = new Carbon($now);
         }
 
-        //user、login_log连表查询，
-        //筛选条件：$start_time<=user.createdAt<=$end_time
-        // and $t_start_time<=login_log.loginTime<=$t_end_time
-        $tomorrow = $day->copy()->addDay();
-        $t_start_time = $tomorrow->startOfDay()->getTimestamp();
-        $t_end_time = $tomorrow->endOfDay()->getTimestamp();
+        //下面统一使用`Carbon::parse($time)`去取对应时间
+        $time = $now->getTimestamp();
 
+        //当天开始时间戳
+        $dayStart = Carbon::parse($time)->subHour()->startOfDay()->getTimestamp();
+        //上一小时结尾时间戳
+        $hourEnd = Carbon::parse($time)->subHour()->endOfHour()->getTimestamp();
+
+        //昨天日期
+        $yesterday = Carbon::parse($time)->subHour()->subDay();
+        //昨天开始时间戳
+        $yesterdayStart = Carbon::parse($time)->subHour()->subDay()->startOfDay()->getTimestamp();
+        //昨天结束时间戳
+        $yesterdayEnd = Carbon::parse($time)->subHour()->subDay()->endOfDay()->getTimestamp();
+
+        //获取"昨日"新注册用户，在"今日"起始到上一小时结尾的登录用户数
         $sql = <<<SQL
 SELECT
 	COUNT( DISTINCT l.userId ) as counts
@@ -95,22 +109,39 @@ FROM
 	t_user_login_log AS l
 	INNER JOIN `t_user` AS u ON l.userId = u.id 
 WHERE
-	l.loginTime >= {$t_start_time} AND l.loginTime <= {$t_end_time}
-	AND u.createdAt >= {$start_time} AND u.createdAt <= {$end_time}
+	l.loginTime >= {$dayStart} AND l.loginTime <= {$hourEnd}
+	AND u.createdAt >= {$yesterdayStart} AND u.createdAt <= {$yesterdayEnd}
 SQL;
-        $login_count = Yii::$app->db->createCommand($sql)->queryScalar();
-        $rate = bcdiv($login_count * 100, $count, 2);
 
+        $loginNum = Yii::$app->db->createCommand($sql)->queryScalar();
+
+        //插入或更新对应日期的记录
         $sql = <<<SQL
-INSERT INTO `t_user_day_retention_rate` ( `day`, `rate`, `created_at`, `updated_at` )
+INSERT INTO `t_user_day_retention_rate` ( `day`, `register_num`, `login_num`, `created_at`, `updated_at` )
 VALUES
-	( '{$day->toDateString()}', {$rate}, {$time}, {$time} ) 
-	ON DUPLICATE KEY UPDATE rate = {$rate},
-	updated_at = {$time}
+	( '{$yesterday->toDateString()}', 0, {$loginNum}, {$time}, {$time} ) 
+	ON DUPLICATE KEY UPDATE login_num = {$loginNum}, updated_at = {$time}
 SQL;
+
         Yii::$app->db->createCommand($sql)
             ->execute();
+
         return 0;
     }
 
+    /**
+     * 临时脚本
+     */
+    public function actionTempLogin(string $start)
+    {
+        $start = (new Carbon($start))->startOfDay();
+        while (true) {
+            if ($start->isToday()) {
+                break;
+            }
+
+            $start = $start->addHour();
+            $this->actionHourLogin($start);
+        }
+    }
 }
